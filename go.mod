ea2fab16871ac6865082927e2710e038b0783d7c
Norronas <nemo.cazin@gmail.com>
2025-07-30 21:49:11 +0200

    [add] : basic config

---
+++ go.mod
@@ -0,0 +1,3 @@
+module f1term
+
+go 1.24.5
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	url := "https://api.openf1.org/v1/meetings?year=2024"
+
+	// Création de la requête
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Erreur lors de la requête:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Lecture de la réponse
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Erreur lors de la lecture de la réponse:", err)
+		return
+	}
+
+	fmt.Println("Réponse de l'API:")
+	fmt.Println(string(body))
+}
